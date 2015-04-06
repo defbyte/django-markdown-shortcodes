e84a0877c8bbd47c1d8de75c6d82069dc3866132
Chris Davis <defbyte@gmail.com>
2015-04-06 13:05:16 -0400

    Fix recursion bug in templatetag.

--- markdown_shortcodes/templatetags/shortcodes.py
+++ markdown_shortcodes/templatetags/shortcodes.py
@@ -1,11 +1,11 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
-from markdown_shortcodes import expand_shortcodes
+from markdown_shortcodes import expand_shortcodes as expand_shortcodes_func
 
 register = template.Library()
 
 @register.filter
 @stringfilter
 def expand_shortcodes(val):
-    return expand_shortcodes(val)
+    return expand_shortcodes_func(val)
