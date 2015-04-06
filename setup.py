aa9516840b02c6e20f677034841de59544604c86
Chris Davis <defbyte@gmail.com>
2015-04-06 12:51:16 -0400

    Freaking templatetags wasn't included in initial release.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -10,8 +10,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-markdown-shortcodes',
-    version='1.0',
-    packages=['markdown_shortcodes'],
+    version='1.1',
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='Utility adding WordPress-like shortcodes for Markdown authoring in Django.',
